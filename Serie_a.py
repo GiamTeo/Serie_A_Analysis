0747707631b3f5dcedf825bd7d446753a4f1873a
GiamTeo <116298717+GiamTeo@users.noreply.github.com>
2024-01-21 10:49:00 +0100

    Update file .py

--- Serie_a.py
+++ Serie_a.py
@@ -1,4 +1,6 @@
 import numpy as np
 import pandas as pd
-df = pd.read_csv("F:\Serie_A_Analysis\matches_serie_2022-23.csv")
+df = pd.read_csv("C:/Users/giamm/OneDrive\Desktop/Giammy/Serie_A_Analysis/Serie_A_Analysis/matches_serie_2022-23.csv")
 # print(df)
+print(df.loc[:,["team_name_home","team_name_away","team_home_score","team_away_score"]])
+# print(df.columns)
